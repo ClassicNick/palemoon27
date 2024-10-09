0eb6f67dbbc85fb71288da79233be545e40b0cb6
Riccardo Mottola <rm@gnu.org>
2024-10-09 10:16:50 +0200

    Bug 1274922 part 1 - Remove ShellRuntime::gotError. r=jonco

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -167,7 +167,6 @@ struct ShellRuntime
 
     int exitCode;
     bool quitting;
-    bool gotError;
 };
 
 // Shell state set once at startup.
@@ -308,8 +307,7 @@ ShellRuntime::ShellRuntime(JSRuntime* rt)
     watchdogTimeout(0),
     sleepWakeup(nullptr),
     exitCode(0),
-    quitting(false),
-    gotError(false)
+    quitting(false)
 {}
 
 static ShellRuntime*
@@ -513,9 +511,7 @@ RunFile(JSContext* cx, const char* filename, FILE* file, bool compileOnly)
                .setIsRunOnce(true)
                .setNoScriptRval(true);
 
-        sr->gotError = false;
         (void) JS::Compile(cx, options, file, &script);
-        MOZ_ASSERT_IF(!script, sr->gotError);
     }
 
     #ifdef DEBUG
@@ -5857,7 +5853,7 @@ js::shell::my_ErrorReporter(JSContext* cx, const char* message, JSErrorReport* r
     if (JS_IsExceptionPending(cx))
         (void) JS_GetPendingException(cx, &exn);
 
-    sr->gotError = PrintError(cx, fp, message, report, reportWarnings);
+    (void) PrintError(cx, fp, message, report, reportWarnings);
     if (!exn.isUndefined()) {
         JS::AutoSaveExceptionState savedExc(cx);
         if (!PrintStackTrace(cx, exn))
@@ -5871,16 +5867,6 @@ js::shell::my_ErrorReporter(JSContext* cx, const char* message, JSErrorReport* r
         else
             sr->exitCode = EXITCODE_RUNTIME_ERROR;
     }
-}
-
-static void
-my_OOMCallback(JSContext* cx, void* data)
-{
-    // If a script is running, the engine is about to throw the string "out of
-    // memory", which may or may not be caught. Otherwise the engine will just
-    // unwind and return null/false, with no exception set.
-    if (!JS_IsRunning(cx))
-        GetShellRuntime(cx)->gotError = true;
 }
 
 static bool
@@ -7304,7 +7290,6 @@ main(int argc, char** argv, char** envp)
     // Waiting is allowed on the shell's main thread, for now.
     JS_SetFutexCanWait(rt);
     JS_SetErrorReporter(rt, my_ErrorReporter);
-    JS::SetOutOfMemoryCallback(rt, my_OOMCallback, nullptr);
     if (!SetRuntimeOptions(rt, op))
         return 1;
 
