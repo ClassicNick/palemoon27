9a2c85576574b4c229e0dde73cf08fbae9c0ca89
Riccardo Mottola <rm@gnu.org>
2024-10-14 23:55:11 +0200

    Bug 1276276 part 6. Get rid of nsJSContext::mContext. r=smaug

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -150,9 +150,8 @@ protected:
   nsresult AddSupportsPrimitiveTojsvals(nsISupports *aArg, JS::Value *aArgv);
 
 private:
-  void DestroyJSContext();
+  void Destroy();
 
-  JSContext *mContext;
   JS::Heap<JSObject*> mWindowProxy;
 
   bool mIsInitialized;
