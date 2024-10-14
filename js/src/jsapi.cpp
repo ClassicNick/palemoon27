dc4ef75f23953d5c42c87f001a8e94d3f0b43943
Riccardo Mottola <rm@gnu.org>
2024-10-14 08:07:52 +0200

    Bug 1274193 part 1 - Pass SavedOption explicitly to frame iterators. r=luke

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -6428,7 +6428,7 @@ DescribeScriptedCaller(JSContext* cx, AutoFilename* filename, unsigned* lineno,
     if (column)
         *column = 0;
 
-    NonBuiltinFrameIter i(cx);
+    NonBuiltinFrameIter i(cx, FrameIter::STOP_AT_SAVED);
     if (i.done())
         return false;
 
@@ -6508,7 +6508,7 @@ GetScriptedCallerGlobal(JSContext* cx)
         if (!activation)
             return nullptr;
     } else {
-        NonBuiltinFrameIter i(cx);
+        NonBuiltinFrameIter i(cx, FrameIter::STOP_AT_SAVED);
         if (i.done())
             return nullptr;
         activation = i.activation();
