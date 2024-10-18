381edc29c026b3d6c79ff267295237b7cf265cf6
Riccardo Mottola <rm@gnu.org>
2024-10-18 18:54:59 +0200

    bug 1270795 - do not generate any background items in glyph path generating process; r=jfkthame

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -2436,6 +2436,10 @@ nsDisplayBackgroundImage::AppendBackgroundItemsToTop(nsDisplayListBuilder* aBuil
                                                      nsDisplayList* aList,
                                                      bool aAllowWillPaintBorderOptimization)
 {
+  if (aBuilder->IsForGenerateGlyphPath()) {
+    return true;
+  }
+
   nsStyleContext* bgSC = nullptr;
   const nsStyleBackground* bg = nullptr;
   nsRect bgRect = aBackgroundRect + aBuilder->ToReferenceFrame(aFrame);
