78fee508268a1a670c88c982bd33d6fa8b43d9db
Riccardo Mottola <rm@gnu.org>
2024-10-22 12:25:55 +0200

    Bug 1274881 - Get rid of imgLoader::GetInstance. r=tnikkel

--- image/imgLoader.h
+++ image/imgLoader.h
@@ -242,9 +242,6 @@ public:
       return loader.forget().downcast<imgLoader>();
   }
 
-  static already_AddRefed<imgLoader>
-  GetInstance();
-
   nsresult LoadImage(nsIURI* aURI,
                      nsIURI* aInitialDocumentURI,
                      nsIURI* aReferrerURI,
