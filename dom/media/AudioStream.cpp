44453d9028213fff7b30399b872f04f3a34ce8fe
Riccardo Mottola <rm@gnu.org>
2024-10-24 17:10:12 +0200

    Bug 1276569. Part 1 - remove null checks. r=kinetik.

--- dom/media/AudioStream.cpp
+++ dom/media/AudioStream.cpp
@@ -420,9 +420,6 @@ void
 AudioStream::StartUnlocked()
 {
   mMonitor.AssertCurrentThreadOwns();
-  if (!mCubebStream) {
-    return;
-  }
 
   if (mState == INITIALIZED) {
     mState = STARTED;
@@ -450,7 +447,7 @@ AudioStream::Pause()
     return;
   }
 
-  if (!mCubebStream || (mState != STARTED && mState != RUNNING)) {
+  if (mState != STARTED && mState != RUNNING) {
     mState = STOPPED; // which also tells async OpenCubeb not to start, just init
     return;
   }
@@ -469,7 +466,7 @@ void
 AudioStream::Resume()
 {
   MonitorAutoLock mon(mMonitor);
-  if (!mCubebStream || mState != STOPPED) {
+  if (mState != STOPPED) {
     return;
   }
 
@@ -520,7 +517,7 @@ AudioStream::GetPositionInFramesUnlocked()
 {
   mMonitor.AssertCurrentThreadOwns();
 
-  if (!mCubebStream || mState == ERRORED) {
+  if (mState == ERRORED) {
     return -1;
   }
 
