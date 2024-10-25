1db80b4d115fe15905a2fd8bb79fd0983c8fc64b
Riccardo Mottola <rm@gnu.org>
2024-10-25 22:12:14 +0200

    Move TaskFactory from dom/plugins to ipc/glue. (bug 1271180 part 1, r=aklotz)

--- dom/plugins/ipc/PluginModuleParent.h
+++ dom/plugins/ipc/PluginModuleParent.h
@@ -15,7 +15,7 @@
 #include "mozilla/plugins/PPluginModuleParent.h"
 #include "mozilla/plugins/PluginMessageUtils.h"
 #include "mozilla/plugins/PluginTypes.h"
-#include "mozilla/plugins/TaskFactory.h"
+#include "mozilla/ipc/TaskFactory.h"
 #include "mozilla/TimeStamp.h"
 #include "npapi.h"
 #include "npfunctions.h"
@@ -321,7 +321,7 @@ protected:
     NPNetscapeFuncs* mNPNIface;
     NPPluginFuncs* mNPPIface;
     nsNPAPIPlugin* mPlugin;
-    TaskFactory<PluginModuleParent> mTaskFactory;
+    ipc::TaskFactory<PluginModuleParent> mTaskFactory;
     nsString mPluginDumpID;
     nsString mBrowserDumpID;
     nsString mHangID;
@@ -539,7 +539,7 @@ private:
     PluginProcessParent* mSubprocess;
     uint32_t mPluginId;
 
-    TaskFactory<PluginModuleChromeParent> mChromeTaskFactory;
+    ipc::TaskFactory<PluginModuleChromeParent> mChromeTaskFactory;
 
     enum HangAnnotationFlags
     {
