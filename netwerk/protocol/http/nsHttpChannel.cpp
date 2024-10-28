62ae388f80ed42b24073696c0e5605d00354f364
Riccardo Mottola <rm@gnu.org>
2024-10-28 14:34:44 +0100

    Bug 1269448 - Fix test assertions. r=mayhemer

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -1635,7 +1635,7 @@ nsHttpChannel::ProcessResponse()
     }
     if (referrer) {
         nsCOMPtr<nsILoadContextInfo> lci = GetLoadContextInfo(this);
-        mozilla::net::Predictor::UpdateCacheability(mReferrer, mURI, httpStatus,
+        mozilla::net::Predictor::UpdateCacheability(referrer, mURI, httpStatus,
                                                     mRequestHead, mResponseHead,
                                                     lci);
     }
