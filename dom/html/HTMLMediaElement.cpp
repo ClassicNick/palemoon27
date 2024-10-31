86bb89b2ab9e125456d170beefe43a61b0597317
Riccardo Mottola <rm@gnu.org>
2024-10-31 11:13:06 +0100

    Bug 1270614 - Rename ReportMSETelemetry(). r=SingingTree
    This function is no longer MSE-specific, so use a more general
    ReportTelemetry() name for it.

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -2772,7 +2772,7 @@ nsresult HTMLMediaElement::BindToTree(nsIDocument* aDocument, nsIContent* aParen
 }
 
 void
-HTMLMediaElement::ReportMSETelemetry()
+HTMLMediaElement::ReportTelemetry()
 {
   // Report telemetry for videos when a page is unloaded. We
   // want to know data on what state the video is at when
@@ -4421,7 +4421,7 @@ void HTMLMediaElement::SuspendOrResumeElement(bool aPauseElement, bool aSuspendE
     UpdateAudioChannelPlayingState();
     if (aPauseElement) {
       if (mMediaSource) {
-        ReportMSETelemetry();
+        ReportTelemetry();
       }
 
       if (mDecoder) {
