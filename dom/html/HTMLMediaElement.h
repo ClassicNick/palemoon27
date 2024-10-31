9df8361fb65a9b78725b0a01e8252cd572ea1aaa
Riccardo Mottola <rm@gnu.org>
2024-10-31 13:16:19 +0100

    Bug 882717 - Invoke TimeMarchesOn when the startTime/endTime/pauseOnExit be changed of TextTrackCue. r=rillian

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -716,6 +716,11 @@ public:
       mTextTrackManager->NotifyCueRemoved(aCue);
     }
   }
+  void NotifyCueUpdated(TextTrackCue *aCue) {
+    if (mTextTrackManager) {
+      mTextTrackManager->NotifyCueUpdated(aCue);
+    }
+  }
 
   /**
    * A public wrapper for FinishDecoderSetup()
