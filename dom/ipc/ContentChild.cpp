4495574b1215aec850850bdc0a4d58174ee558a2
Riccardo Mottola <rm@gnu.org>
2024-11-04 17:45:13 +0100

    Bug 1273998 - Proper export of printing IPDL with --disable-printing r=bobowen,glandium,?glandium

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -106,7 +106,9 @@
 #include "nsDirectoryServiceUtils.h"
 #include "nsDirectoryServiceDefs.h"
 #include "nsContentPermissionHelper.h"
+#ifdef NS_PRINTING
 #include "nsPrintingProxy.h"
+#endif
 
 #include "IHistory.h"
 #include "nsNetUtil.h"
