f4efd2e67efe9ead04bc1ddac5ca073e7ca35c62
Riccardo Mottola <rm@gnu.org>
2024-11-07 09:13:59 +0100

    Bug 1270797 - Differentiate between nsDisplayBlendContainers created for mix-blend-mode and background-blend-mode. r=mstange

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -2434,8 +2434,8 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
     DisplayListClipState::AutoSaveRestore blendContainerClipState(aBuilder);
     blendContainerClipState.Clear();
     resultList.AppendNewToTop(
-      new (aBuilder) nsDisplayBlendContainer(aBuilder, this, &resultList,
-                                             containerItemScrollClip));
+      nsDisplayBlendContainer::CreateForMixBlendMode(aBuilder, this, &resultList,
+                                                     containerItemScrollClip));
   }
 
   if (!isTransformed && !useFixedPosition && !useStickyPosition) {
