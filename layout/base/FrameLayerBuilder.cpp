b0bbf63e8a9ec13833470b7ab4c6c9676672a7a4
Riccardo Mottola <rm@gnu.org>
2024-11-07 12:52:01 +0100

    Bug 1153694 - Don't let nsDisplayFixedPosition cause unnecessary layerization around itself. r=mattwoodrow

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -3999,6 +3999,10 @@ ContainerState::ProcessDisplayItems(nsDisplayList* aList)
         mPaintedLayerDataTree.AddingOwnLayer(animatedGeometryRoot,
                                              clipPtr,
                                              uniformColorPtr);
+      } else if (!clipMovesWithLayer) {
+        mPaintedLayerDataTree.AddingOwnLayer(animatedGeometryRootForClip,
+                                             clipPtr,
+                                             uniformColorPtr);
       } else {
         // Using itemVisibleRect here isn't perfect. itemVisibleRect can be
         // larger or smaller than the potential bounds of item's contents in
