06c10766873246ea289277185abe4a2338edfb21
Riccardo Mottola <rm@gnu.org>
2024-11-07 10:18:45 +0100

    Bug 1273024 - Stop inflating the visible rect for fixed-position items unnecessarily. r=mattwoodrow

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -4986,8 +4986,6 @@ nsDisplayFixedPosition::nsDisplayFixedPosition(nsDisplayListBuilder* aBuilder,
 void
 nsDisplayFixedPosition::Init(nsDisplayListBuilder* aBuilder)
 {
-  bool snap;
-  mVisibleRect = GetBounds(aBuilder, &snap);
   mAnimatedGeometryRootForScrollMetadata = mAnimatedGeometryRoot;
   if (ShouldFixToViewport(aBuilder)) {
     mAnimatedGeometryRoot = aBuilder->FindAnimatedGeometryRootFor(this);
