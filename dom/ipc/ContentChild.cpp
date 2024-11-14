a9fd67da2754f2a46590e5cbc248c6ac1e306c85
Riccardo Mottola <rm@gnu.org>
2024-11-14 12:55:53 +0100

    Bug 1274516 - Add a helper to iterate over all ContentParents and use it to clear the image cache in both the parent and child processes. r=billm r=seth

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -58,6 +58,7 @@
 #include "mozilla/widget/WidgetMessageUtils.h"
 #include "nsBaseDragService.h"
 #include "mozilla/media/MediaChild.h"
+#include "imgLoader.h"
 
 #if defined(MOZ_CONTENT_SANDBOX)
 #if defined(XP_WIN)
@@ -2221,6 +2222,16 @@ ContentChild::RecvRegisterChromeItem(const ChromeRegistryItem& item)
       return false;
   }
 
+  return true;
+}
+
+bool
+ContentChild::RecvClearImageCache(const bool& privateLoader, const bool& chrome)
+{
+  imgLoader* loader = privateLoader ? imgLoader::PrivateBrowsingLoader() :
+                                      imgLoader::NormalLoader();
+
+  loader->ClearCache(chrome);
   return true;
 }
 
--- dom/ipc/ContentChild.h
+++ dom/ipc/ContentChild.h
@@ -377,6 +377,9 @@ public:
 
   virtual bool RecvRegisterChromeItem(const ChromeRegistryItem& item) override;
 
+  virtual bool RecvClearImageCache(const bool& privateLoader,
+                                   const bool& chrome) override;
+
   virtual mozilla::jsipc::PJavaScriptChild* AllocPJavaScriptChild() override;
 
   virtual bool DeallocPJavaScriptChild(mozilla::jsipc::PJavaScriptChild*) override;
