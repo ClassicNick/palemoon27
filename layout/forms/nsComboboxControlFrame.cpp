6320dd01d621e0a2394e42b34215787f4898c6d6
Riccardo Mottola <rm@gnu.org>
2024-11-15 13:28:59 +0100

    Bug 1024350 - Support fire input event for select element.r=smaug

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -288,7 +288,7 @@ nsComboboxControlFrame::SetFocus(bool aOn, bool aRepaint)
       }
     }
     // May delete |this|.
-    mListControlFrame->FireOnChange();
+    mListControlFrame->FireOnInputAndOnChange();
   }
 
   if (!weakFrame.IsAlive()) {
