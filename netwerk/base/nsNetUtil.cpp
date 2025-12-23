112234123b8432158530b4427ed8d9133356f487
Nicholas McAnespy <ClassicNick@github.com>
2025-12-22 17:06:14 -0800

    Revert "Bug 1270678 - Part 1: Make the NS_GetOriginAttributes() acquires originAttributes from the loadInfo when loadContext is not available. r=valentin"
    This reverts commit df57062cfabc17610fe64e44dd51c2c11ed4a9a5.

--- netwerk/base/nsNetUtil.cpp
+++ netwerk/base/nsNetUtil.cpp
@@ -1266,27 +1266,12 @@ NS_GetOriginAttributes(nsIChannel *aChannel,
                        mozilla::OriginAttributes &aAttributes)
 {
     nsCOMPtr<nsILoadContext> loadContext;
-    nsCOMPtr<nsILoadInfo> loadInfo = aChannel->GetLoadInfo();
     NS_QueryNotificationCallbacks(aChannel, loadContext);
-
-    if (!loadContext && !loadInfo) {
+    if (!loadContext) {
         return false;
     }
 
     loadContext->GetOriginAttributes(aAttributes);
-
-    // Bug 1270678 - By default, we would acquire the originAttributes from
-    // the loadContext. But in some cases, say, loading the favicon, that the
-    // loadContext is not available. We would use the loadInfo to get
-    // originAttributes instead.
-    if (loadContext) {
-        DocShellOriginAttributes doa;
-        loadContext->GetOriginAttributes(doa);
-        aAttributes.InheritFromDocShellToNecko(doa);
-    } else {
-        loadInfo->GetOriginAttributes(&aAttributes);
-    }
-    aAttributes.SyncAttributesWithPrivateBrowsing(NS_UsePrivateBrowsing(aChannel));
     return true;
 }
 
