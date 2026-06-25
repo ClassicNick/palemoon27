rmottola/Arctic-Fox

Bug 1263991 parts 2 and 2.5: Enable launching a server in the parent process. r=baku,ehsan

dom/flyweb/FlyWebPublishedServerIPC.h
@@ -1,0 +1,141 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_FlyWebPublishedServerIPC_h
+#define mozilla_dom_FlyWebPublishedServerIPC_h
+
+#include "HttpServer.h"
+#include "mozilla/dom/FlyWebPublishedServer.h"
+#include "mozilla/dom/PFlyWebPublishedServerParent.h"
+#include "mozilla/dom/PFlyWebPublishedServerChild.h"
+#include "mozilla/MozPromise.h"
+#include "nsISupportsImpl.h"
+#include "nsICancelable.h"
+
+class nsPIDOMWindowInner;
+
+namespace mozilla {
+namespace dom {
+
+class FlyWebPublishedServerParent;
+
+class FlyWebPublishedServerImpl final : public FlyWebPublishedServer
+                                      , public HttpServerListener
+{
+public:
+  FlyWebPublishedServerImpl(nsPIDOMWindowInner* aOwner,
+                            const nsAString& aName,
+                            const FlyWebPublishOptions& aOptions);
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  int32_t Port()
+  {
+    return mHttpServer ? mHttpServer->GetPort() : 0;
+  }
+  void GetCertKey(nsACString& aKey) {
+    if (mHttpServer) {
+      mHttpServer->GetCertKey(aKey);
+    } else {
+      aKey.Truncate();
+    }
+  }
+
+  virtual void OnFetchResponse(InternalRequest* aRequest,
+                               InternalResponse* aResponse) override;
+  virtual already_AddRefed<WebSocket>
+    OnWebSocketAccept(InternalRequest* aConnectRequest,
+                      const Optional<nsAString>& aProtocol,
+                      ErrorResult& aRv) override;
+  virtual void OnWebSocketResponse(InternalRequest* aConnectRequest,
+                                   InternalResponse* aResponse) override;
+
+  void SetCancelRegister(nsICancelable* aCancelRegister)
+  {
+    mMDNSCancelRegister = aCancelRegister;
+  }
+
+  virtual void Close() override;
+
+  // HttpServerListener
+  virtual void OnServerStarted(nsresult aStatus) override;
+  virtual void OnRequest(InternalRequest* aRequest) override
+  {
+    FireFetchEvent(aRequest);
+  }
+  virtual void OnWebSocket(InternalRequest* aConnectRequest) override
+  {
+    FireWebsocketEvent(aConnectRequest);
+  }
+  virtual void OnServerClose() override
+  {
+    mHttpServer = nullptr;
+    Close();
+  }
+
+private:
+  ~FlyWebPublishedServerImpl() {}
+
+  RefPtr<HttpServer> mHttpServer;
+  nsCOMPtr<nsICancelable> mMDNSCancelRegister;
+  RefPtr<FlyWebPublishedServerParent> mServerParent;
+};
+
+class FlyWebPublishedServerChild final : public FlyWebPublishedServer
+                                       , public PFlyWebPublishedServerChild
+{
+public:
+  FlyWebPublishedServerChild(nsPIDOMWindowInner* aOwner,
+                             const nsAString& aName,
+                             const FlyWebPublishOptions& aOptions);
+
+  virtual bool RecvServerReady(const nsresult& aStatus) override;
+  virtual bool RecvServerClose() override;
+
+  virtual void OnFetchResponse(InternalRequest* aRequest,
+                               InternalResponse* aResponse) override;
+  virtual already_AddRefed<WebSocket>
+    OnWebSocketAccept(InternalRequest* aConnectRequest,
+                      const Optional<nsAString>& aProtocol,
+                      ErrorResult& aRv) override;
+  virtual void OnWebSocketResponse(InternalRequest* aConnectRequest,
+                                   InternalResponse* aResponse) override;
+
+  virtual void Close() override;
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) override;
+
+private:
+  ~FlyWebPublishedServerChild() {}
+
+  bool mActorDestroyed;
+};
+
+class FlyWebPublishedServerParent final : public PFlyWebPublishedServerParent
+{
+public:
+  FlyWebPublishedServerParent(const nsAString& aName,
+                              const FlyWebPublishOptions& aOptions);
+
+  NS_INLINE_DECL_REFCOUNTING(FlyWebPublishedServerParent)
+
+private:
+  virtual void
+  ActorDestroy(ActorDestroyReason aWhy) override;
+
+  virtual bool
+  Recv__delete__() override;
+
+  ~FlyWebPublishedServerParent() {}
+
+  bool mActorDestroyed;
+  RefPtr<FlyWebPublishedServerImpl> mPublishedServer;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_FlyWebPublishedServerIPC_h

dom/flyweb/FlyWebService.cpp
@@ -8,7 +8,7 @@
 #include "mozilla/StaticPtr.h"
 #include "mozilla/ScopeExit.h"
 #include "mozilla/dom/Promise.h"
-#include "mozilla/dom/FlyWebPublishedServer.h"
+#include "mozilla/dom/FlyWebPublishedServerIPC.h"
 #include "nsISocketTransportService.h"
 #include "mdns/libmdns/nsDNSServiceInfo.h"
 #include "nsIUUIDGenerator.h"
@@ -77,7 +77,7 @@ private:
   nsresult PairWithService(const nsAString& aServiceId,
                            UniquePtr<FlyWebService::PairedInfo>& aInfo);
 
-  nsresult StartDiscoveryOf(FlyWebPublishedServer* aServer);
+  nsresult StartDiscoveryOf(FlyWebPublishedServerImpl* aServer);
 
   void EnsureDiscoveryStarted();
   void EnsureDiscoveryStopped();
@@ -442,7 +442,7 @@ FlyWebMDNSService::OnServiceRegistered(nsIDNSServiceInfo* aServiceInfo)
     return NS_ERROR_FAILURE;
   }
 
-  existingServer->DiscoveryStarted(NS_OK);
+  existingServer->PublishedServerStarted(NS_OK);
 
   return NS_OK;
 }
@@ -489,7 +489,7 @@ FlyWebMDNSService::OnRegistrationFailed(nsIDNSServiceInfo* aServiceInfo, int32_t
   LOG_I("OnServiceRegistered(MDNS): Registration of server with name %s failed.", cName.get());
 
   // Remove the nsICancelable from the published server.
-  existingServer->DiscoveryStarted(NS_ERROR_FAILURE);
+  existingServer->PublishedServerStarted(NS_ERROR_FAILURE);
   return NS_OK;
 }
 
@@ -696,7 +696,7 @@ FlyWebMDNSService::PairWithService(const nsAString& aServiceId,
 }
 
 nsresult
-FlyWebMDNSService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
+FlyWebMDNSService::StartDiscoveryOf(FlyWebPublishedServerImpl* aServer)
 {
 
   RefPtr<FlyWebPublishedServer> existingServer =
@@ -805,6 +805,14 @@ FlyWebService::GetOrCreate()
 ErrorResult
 FlyWebService::Init()
 {
+  // Most functions of FlyWebService should not be started in the child.
+  // Instead FlyWebService in the child is mainly responsible for tracking
+  // publishedServer lifetimes. Other functions are handled by the
+  // FlyWebService running in the parent.
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    return ErrorResult(NS_OK);
+  }
+
   MOZ_ASSERT(NS_IsMainThread());
   if (!mMDNSHttpService) {
     mMDNSHttpService = new FlyWebMDNSService(this, NS_LITERAL_CSTRING("_http._tcp."));
@@ -833,19 +841,11 @@ FlyWebService::Init()
   return ErrorResult(NS_OK);
 }
 
-already_AddRefed<Promise>
+already_AddRefed<FlyWebPublishPromise>
 FlyWebService::PublishServer(const nsAString& aName,
                              const FlyWebPublishOptions& aOptions,
-                             nsPIDOMWindowInner* aWindow,
-                             ErrorResult& aRv)
+                             nsPIDOMWindowInner* aWindow)
 {
-  MOZ_ASSERT(NS_IsMainThread());
-  nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aWindow);
-  RefPtr<Promise> promise = Promise::Create(global, aRv);
-  if (aRv.Failed()) {
-    return nullptr;
-  }
-
   // Scan uiUrl for illegal characters
 
   RefPtr<FlyWebPublishedServer> existingServer =
@@ -853,16 +853,22 @@ FlyWebService::PublishServer(const nsAString& aName,
   if (existingServer) {
     LOG_I("PublishServer: Trying to publish server with already-existing name %s.",
           NS_ConvertUTF16toUTF8(aName).get());
-    promise->MaybeReject(NS_ERROR_FAILURE);
+    MozPromiseHolder<FlyWebPublishPromise> holder;
+    RefPtr<FlyWebPublishPromise> promise = holder.Ensure(__func__);
+    holder.Reject(NS_ERROR_FAILURE, __func__);
     return promise.forget();
   }
 
-  RefPtr<FlyWebPublishedServer> server =
-    new FlyWebPublishedServer(aWindow, aName, aOptions, promise);
+  RefPtr<FlyWebPublishedServer> server;
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    server = new FlyWebPublishedServerChild(aWindow, aName, aOptions);
+  } else {
+    server = new FlyWebPublishedServerImpl(aWindow, aName, aOptions);
+  }
 
   mServers.AppendElement(server);
 
-  return promise.forget();
+  return server->GetPublishPromise();
 }
 
 already_AddRefed<FlyWebPublishedServer>
@@ -1111,7 +1117,7 @@ FlyWebService::CreateTransportForHost(const char **types,
 }
 
 void
-FlyWebService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
+FlyWebService::StartDiscoveryOf(FlyWebPublishedServerImpl* aServer)
 {
   MOZ_ASSERT(NS_IsMainThread());
   nsresult rv = mMDNSFlywebService ?
@@ -1119,7 +1125,7 @@ FlyWebService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
     NS_ERROR_FAILURE;
 
   if (NS_FAILED(rv)) {
-    aServer->DiscoveryStarted(rv);
+    aServer->PublishedServerStarted(rv);
   }
 }
 

ClassicNick/palemoon27

dom/flyweb/FlyWebPublishedServerIPC.h
@@ -1,0 +1,141 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_FlyWebPublishedServerIPC_h
+#define mozilla_dom_FlyWebPublishedServerIPC_h
+
+#include "HttpServer.h"
+#include "mozilla/dom/FlyWebPublishedServer.h"
+#include "mozilla/dom/PFlyWebPublishedServerParent.h"
+#include "mozilla/dom/PFlyWebPublishedServerChild.h"
+#include "mozilla/MozPromise.h"
+#include "nsISupportsImpl.h"
+#include "nsICancelable.h"
+
+class nsPIDOMWindowInner;
+
+namespace mozilla {
+namespace dom {
+
+class FlyWebPublishedServerParent;
+
+class FlyWebPublishedServerImpl final : public FlyWebPublishedServer
+                                      , public HttpServerListener
+{
+public:
+  FlyWebPublishedServerImpl(nsPIDOMWindow* aOwner,
+                            const nsAString& aName,
+                            const FlyWebPublishOptions& aOptions);
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  int32_t Port()
+  {
+    return mHttpServer ? mHttpServer->GetPort() : 0;
+  }
+  void GetCertKey(nsACString& aKey) {
+    if (mHttpServer) {
+      mHttpServer->GetCertKey(aKey);
+    } else {
+      aKey.Truncate();
+    }
+  }
+
+  virtual void OnFetchResponse(InternalRequest* aRequest,
+                               InternalResponse* aResponse) override;
+  virtual already_AddRefed<WebSocket>
+    OnWebSocketAccept(InternalRequest* aConnectRequest,
+                      const Optional<nsAString>& aProtocol,
+                      ErrorResult& aRv) override;
+  virtual void OnWebSocketResponse(InternalRequest* aConnectRequest,
+                                   InternalResponse* aResponse) override;
+
+  void SetCancelRegister(nsICancelable* aCancelRegister)
+  {
+    mMDNSCancelRegister = aCancelRegister;
+  }
+
+  virtual void Close() override;
+
+  // HttpServerListener
+  virtual void OnServerStarted(nsresult aStatus) override;
+  virtual void OnRequest(InternalRequest* aRequest) override
+  {
+    FireFetchEvent(aRequest);
+  }
+  virtual void OnWebSocket(InternalRequest* aConnectRequest) override
+  {
+    FireWebsocketEvent(aConnectRequest);
+  }
+  virtual void OnServerClose() override
+  {
+    mHttpServer = nullptr;
+    Close();
+  }
+
+private:
+  ~FlyWebPublishedServerImpl() {}
+
+  RefPtr<HttpServer> mHttpServer;
+  nsCOMPtr<nsICancelable> mMDNSCancelRegister;
+  RefPtr<FlyWebPublishedServerParent> mServerParent;
+};
+
+class FlyWebPublishedServerChild final : public FlyWebPublishedServer
+                                       , public PFlyWebPublishedServerChild
+{
+public:
+  FlyWebPublishedServerChild(nsPIDOMWindow* aOwner,
+                             const nsAString& aName,
+                             const FlyWebPublishOptions& aOptions);
+
+  virtual bool RecvServerReady(const nsresult& aStatus) override;
+  virtual bool RecvServerClose() override;
+
+  virtual void OnFetchResponse(InternalRequest* aRequest,
+                               InternalResponse* aResponse) override;
+  virtual already_AddRefed<WebSocket>
+    OnWebSocketAccept(InternalRequest* aConnectRequest,
+                      const Optional<nsAString>& aProtocol,
+                      ErrorResult& aRv) override;
+  virtual void OnWebSocketResponse(InternalRequest* aConnectRequest,
+                                   InternalResponse* aResponse) override;
+
+  virtual void Close() override;
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) override;
+
+private:
+  ~FlyWebPublishedServerChild() {}
+
+  bool mActorDestroyed;
+};
+
+class FlyWebPublishedServerParent final : public PFlyWebPublishedServerParent
+{
+public:
+  FlyWebPublishedServerParent(const nsAString& aName,
+                              const FlyWebPublishOptions& aOptions);
+
+  NS_INLINE_DECL_REFCOUNTING(FlyWebPublishedServerParent)
+
+private:
+  virtual void
+  ActorDestroy(ActorDestroyReason aWhy) override;
+
+  virtual bool
+  Recv__delete__() override;
+
+  ~FlyWebPublishedServerParent() {}
+
+  bool mActorDestroyed;
+  RefPtr<FlyWebPublishedServerImpl> mPublishedServer;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_FlyWebPublishedServerIPC_h

dom/flyweb/FlyWebService.cpp
@@ -8,7 +8,7 @@
 #include "mozilla/StaticPtr.h"
 #include "mozilla/ScopeExit.h"
 #include "mozilla/dom/Promise.h"
-#include "mozilla/dom/FlyWebPublishedServer.h"
+#include "mozilla/dom/FlyWebPublishedServerIPC.h"
 #include "nsISocketTransportService.h"
 #include "mdns/libmdns/nsDNSServiceInfo.h"
 #include "nsIUUIDGenerator.h"
@@ -25,6 +25,7 @@
 #include "nsISimpleEnumerator.h"
 #include "nsIProperty.h"
 #include "nsICertOverrideService.h"
+#include "nsSocketTransport2.h"
 
 namespace mozilla {
 namespace dom {
@@ -77,7 +78,7 @@ private:
   nsresult PairWithService(const nsAString& aServiceId,
                            UniquePtr<FlyWebService::PairedInfo>& aInfo);
 
-  nsresult StartDiscoveryOf(FlyWebPublishedServer* aServer);
+  nsresult StartDiscoveryOf(FlyWebPublishedServerImpl* aServer);
 
   void EnsureDiscoveryStarted();
   void EnsureDiscoveryStopped();
@@ -442,7 +443,7 @@ FlyWebMDNSService::OnServiceRegistered(nsIDNSServiceInfo* aServiceInfo)
     return NS_ERROR_FAILURE;
   }
 
-  existingServer->DiscoveryStarted(NS_OK);
+  existingServer->PublishedServerStarted(NS_OK);
 
   return NS_OK;
 }
@@ -489,7 +490,7 @@ FlyWebMDNSService::OnRegistrationFailed(nsIDNSServiceInfo* aServiceInfo, int32_t
   LOG_I("OnServiceRegistered(MDNS): Registration of server with name %s failed.", cName.get());
 
   // Remove the nsICancelable from the published server.
-  existingServer->DiscoveryStarted(NS_ERROR_FAILURE);
+  existingServer->PublishedServerStarted(NS_ERROR_FAILURE);
   return NS_OK;
 }
 
@@ -696,7 +697,7 @@ FlyWebMDNSService::PairWithService(const nsAString& aServiceId,
 }
 
 nsresult
-FlyWebMDNSService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
+FlyWebMDNSService::StartDiscoveryOf(FlyWebPublishedServerImpl* aServer)
 {
 
   RefPtr<FlyWebPublishedServer> existingServer =
@@ -805,6 +806,14 @@ FlyWebService::GetOrCreate()
 ErrorResult
 FlyWebService::Init()
 {
+  // Most functions of FlyWebService should not be started in the child.
+  // Instead FlyWebService in the child is mainly responsible for tracking
+  // publishedServer lifetimes. Other functions are handled by the
+  // FlyWebService running in the parent.
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    return ErrorResult(NS_OK);
+  }
+
   MOZ_ASSERT(NS_IsMainThread());
   if (!mMDNSHttpService) {
     mMDNSHttpService = new FlyWebMDNSService(this, NS_LITERAL_CSTRING("_http._tcp."));
@@ -833,19 +842,11 @@ FlyWebService::Init()
   return ErrorResult(NS_OK);
 }
 
-already_AddRefed<Promise>
+already_AddRefed<FlyWebPublishPromise>
 FlyWebService::PublishServer(const nsAString& aName,
                              const FlyWebPublishOptions& aOptions,
-                             nsPIDOMWindowInner* aWindow,
-                             ErrorResult& aRv)
+                             nsPIDOMWindow* aWindow)
 {
-  MOZ_ASSERT(NS_IsMainThread());
-  nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aWindow);
-  RefPtr<Promise> promise = Promise::Create(global, aRv);
-  if (aRv.Failed()) {
-    return nullptr;
-  }
-
   // Scan uiUrl for illegal characters
 
   RefPtr<FlyWebPublishedServer> existingServer =
@@ -853,16 +854,22 @@ FlyWebService::PublishServer(const nsAString& aName,
   if (existingServer) {
     LOG_I("PublishServer: Trying to publish server with already-existing name %s.",
           NS_ConvertUTF16toUTF8(aName).get());
-    promise->MaybeReject(NS_ERROR_FAILURE);
+    MozPromiseHolder<FlyWebPublishPromise> holder;
+    RefPtr<FlyWebPublishPromise> promise = holder.Ensure(__func__);
+    holder.Reject(NS_ERROR_FAILURE, __func__);
     return promise.forget();
   }
 
-  RefPtr<FlyWebPublishedServer> server =
-    new FlyWebPublishedServer(aWindow, aName, aOptions, promise);
+  RefPtr<FlyWebPublishedServer> server;
+  if (XRE_GetProcessType() == GeckoProcessType_Content) {
+    server = new FlyWebPublishedServerChild(aWindow, aName, aOptions);
+  } else {
+    server = new FlyWebPublishedServerImpl(aWindow, aName, aOptions);
+  }
 
   mServers.AppendElement(server);
 
-  return promise.forget();
+  return server->GetPublishPromise();
 }
 
 already_AddRefed<FlyWebPublishedServer>
@@ -945,7 +952,7 @@ FlyWebService::HasConnectionOrServer(uint64_t aWindowID)
 {
   MOZ_ASSERT(NS_IsMainThread());
   for (FlyWebPublishedServer* server : mServers) {
-    nsPIDOMWindowInner* win = server->GetOwner();
+    nsPIDOMWindow* win = server->GetOwner();
     if (win && win->WindowID() == aWindowID) {
       return true;
     }
@@ -1101,7 +1108,7 @@ FlyWebService::CreateTransportForHost(const char **types,
   PRNetAddrToNetAddr(&prNetAddr, &netAddr);
   netAddr.inet.port = htons(discPort);
 
-  RefPtr<mozilla::net::nsSocketTransport> trans = new mozilla::net::nsSocketTransport();
+  RefPtr<nsSocketTransport> trans = new nsSocketTransport();
   nsresult rv = trans->InitPreResolved(
     types, typeCount, host, port, hostRoute, portRoute, proxyInfo, &netAddr);
   NS_ENSURE_SUCCESS(rv, rv);
@@ -1111,7 +1118,7 @@ FlyWebService::CreateTransportForHost(const char **types,
 }
 
 void
-FlyWebService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
+FlyWebService::StartDiscoveryOf(FlyWebPublishedServerImpl* aServer)
 {
   MOZ_ASSERT(NS_IsMainThread());
   nsresult rv = mMDNSFlywebService ?
@@ -1119,7 +1126,7 @@ FlyWebService::StartDiscoveryOf(FlyWebPublishedServer* aServer)
     NS_ERROR_FAILURE;
 
   if (NS_FAILED(rv)) {
-    aServer->DiscoveryStarted(rv);
+    aServer->PublishedServerStarted(rv);
   }
 }
 